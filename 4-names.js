a87933c3f5743134d4b4d109bd8b6f2d29dd5a82
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-22 06:17:10 -0400

    Modules, require, module.export

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = 'John'
+const peter = 'Peter'
+
+// This is the learn how to use the model object
+// console.log(module)
+module.exports = { john, peter }
---
+++ 7-mind-grenade.js
@@ -0,0 +1,9 @@
+const num1 = 5;
+const num2 = 10;
+
+// old style function not arrow function
+function addValues() {
+    console.log(`The sum is: ${num1 + num2}`) // ${} == template literal
+}
+
+addValues()
