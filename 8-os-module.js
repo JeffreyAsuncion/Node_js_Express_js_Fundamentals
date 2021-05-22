366c47bdf0c8bd7c34edb6b74393adf3a9545567
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-22 13:47:08 -0400

    os module

--- 8-os-module.js
+++ 8-os-module.js
@@ -1,3 +1,17 @@
-const path = require('path');
+const os = require('os')    // os is built in , if not build in you have to install
 
-console.log(path.sep)
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+
+// method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`);
+
+const currentOs = {
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem()
+}
+console.log(currentOs)
--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const os = require('os')    // os is built in , if not build in you have to install
-
-// info about current user
-const user = os.userInfo()
-console.log(user)
 
 
-// method returns the system uptime in seconds
-console.log(`The System Uptime is ${os.uptime()} seconds`);
+const path = require('path');
 
-const currentOs = {
-    name:os.type(),
-    release:os.release(),
-    totalMem:os.totalmem(),
-    freeMem:os.freemem()
-}
-console.log(currentOs)
+console.log(path.sep)
