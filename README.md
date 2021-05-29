5e33c571bb1851885103ade4c528a4aaa05494ac
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-29 18:56:29 -0400

    working with npm and package-lock.json

--- README.md
+++ README.md
@@ -8,3 +8,44 @@ to excute
 
 node app.js
 node app
+
+
+
+to create enviroment (like pipenv install --dev)
+to install dependencies for the project
+>>> npm install
+
+
+dependencies for this project
+for local use nodemon, for developement np2
+npm i nodemon -D (or --save-dev)
+
+
+in package.json
+ "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js"     
+  },
+
+to run whole app (i thinks)
+>>> npm start 
+
+to run whole app with reload (--reload)
+>>> npm start dev
+
+NOw modify this to 
+in package.json
+ "scripts": {
+    "start" : "nodemon app.js"     
+  },
+
+
+to uninstall bootstrap
+>>> npm uninstall bootstrap
+
+
+
+To remove pkg manually
+-- kill package-lock.json
+-- delete line of pkg in package json
+>>> npm install
--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const http = require('http')
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.end('Welcome to our home page')
-    }
-    if(req.url === '/about'){
-        res.end('Here is our short history!')
-    }
-    res.end(`
-        <h1>Oops!</h1>
-        <p>We can't see to find the page you are looking for </p>
-        <a href="/">back home</a>
-    `)
-})
-server.listen(5000)
+// local dependency - use it only in this particular projedt
+// npm i <packageName>
+
+// glob dependency - use it in any project
+// npm stall -g <packageName>
+// sudo install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root create propties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything defaults)
+
+const _ = require('lodash')
+
+const items = [1, [2, [3, [4]]]]
+console.log(items)
+const newItems = _.flattenDeep(items)
+console.log(newItems)
+console.log('hello world')
