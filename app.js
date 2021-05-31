87f4b579ee0421a5802ec7c4ed2d9fabab73556c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-30 20:47:59 -0400

    npm and package-lock.json

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-// npm - global command, comes with node
-// npm --version
+console.log('first task')
+setTimeout(() => {
+    console.log('second task')
+}, 0)
 
-// local dependency - use it only in this particular projedt
-// npm i <packageName>
-
-// glob dependency - use it in any project
-// npm stall -g <packageName>
-// sudo install -g <packageName> (mac)
-
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root create propties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything defaults)
-
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4]]]]
-console.log(items)
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log('hello world')
+console.log('next task')
