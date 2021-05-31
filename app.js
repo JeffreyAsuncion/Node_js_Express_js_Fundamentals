91e2aedb29da78f1b7b0e2f5a0251248acc3afc0
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-30 21:16:09 -0400

    event loop - read file

---
+++ app.js
@@ -0,0 +1,16 @@
+// // started operating system process
+// console.log('first')
+// setTimeout(() => {
+//     console.log('second')
+// }, 0)
+// console.log('third')
+// // completed and excited operating system process
+
+
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run first')
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
